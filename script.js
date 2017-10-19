207e1f36bf3dcdfddb9b014f57a33f09d3f234d0
Alejandro Hernandez <alejandro.hernandez@sngular.team>
2017-10-19 10:55:37 -0500

    se escuchan las flechas

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 $( document ).ready(function() {
-	$("#button").click(function(){
-		$("#button").css("margin-left","-=10px")
-	})
-    console.log( "ready!" );
+	$(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: // left
+        $("#button").css("margin-left","-=10px");
+        break;
+
+        case 38: // up
+        $("#button").css("margin-top","-=10px");
+        break;
+
+        case 39: // right
+        $("#button").css("margin-left","+=10px");
+        break;
+
+        case 40: // down
+        $("#button").css("margin-top","+=10px");
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
+    
 });
